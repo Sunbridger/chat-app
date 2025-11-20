54d736e19b30f79b85ec250269b0685278c141fb
七彩 <wangriqiao@cai-inc.com>
2025-11-20 14:02:30 +0800

    refactor(ci): 简化GitHub Pages部署配置

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // eslint-disable-next-line no-undef
-  base: process.env.VITE_GITHUB_PAGES ? '/chat-app/' : '/'
+  base: '/chat-app/'
 })
